7ec0346675ae843766e2bd2f60ec405a8ecb6a28
Rony <ronyrodriguez2011@hotmail.com>
2019-02-11 22:07:39 -0600

    Primer Commit

---
+++ js/controlador.js
@@ -0,0 +1,8 @@
+
+$("#btn-icon0").click(function(){
+
+/*funcion para los iconos del index
+para ello encerrar el i y p en un a y darle la clase btn i asignarle el id btn-icono
+ya dsps darle la funcionalidad de agregar o quitar el display*/
+
+});
