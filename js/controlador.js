a2e8295b0d9bbeffb42c5182ebb8139d5bc2d3aa
Rony <ronyrodriguez2011@hotmail.com>
2019-02-11 22:27:04 -0600

    Solo una idea

--- js/controlador.js
+++ js/controlador.js
@@ -3,6 +3,18 @@ $("#btn-icon0").click(function(){
 
 /*funcion para los iconos del index
 para ello encerrar el i y p en un a y darle la clase btn i asignarle el id btn-icono
-ya dsps darle la funcionalidad de agregar o quitar el display*/
+ya dsps darle la funcionalidad de agregar o quitar el display
+
+la funcion podria ser algo como:
+
+funcion(id-del-contenido-a-ocultar/mostrar)
+
+if(id==tal)
+
+ocultar estos dos y mostrar el del id
+
+y asi con los demas ids
+
+*/
 
 });
