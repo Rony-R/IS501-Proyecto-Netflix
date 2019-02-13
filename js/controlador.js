d0c0607f14cbb9c40a7393d1963d0130522c9316
Rony <ronyrodriguez2011@hotmail.com>
2019-02-13 00:20:48 -0600

    Pagina principal terminada

--- js/controlador.js
+++ js/controlador.js
@@ -1,26 +1,4 @@
 
-$("#btn-icon0").click(function(){
-
-
-
-/*funcion para los iconos del index
-para ello encerrar el i y p en un a y darle la clase btn i asignarle el id btn-icono
-ya dsps darle la funcionalidad de agregar o quitar el display
-
-la funcion podria ser algo como:
-
-funcion(id-del-contenido-a-ocultar/mostrar)
-
-if(id==tal)
-
-ocultar estos dos y mostrar el del id
-
-y asi con los demas ids
-
-*/
-
-});
-
 function contenidoIcono(idContenido){
     
     if(idContenido == "door-icon")
@@ -51,14 +29,4 @@ function contenidoIcono(idContenido){
             $("#tv-icon").removeClass("d-block");
         }
     }
-}
-
-function contenidoIcono2(idContenido2){
-
-    $("#" + idContenido2).addClass("d-flex");
-    $("#tv-icon").addClass("d-none");
-    $("#tag-icon").addClass("d-none");
-    $("#tv-icon").removeClass("d-block");
-    $("#tag-icon").removeClass("d-block");
-
 }
