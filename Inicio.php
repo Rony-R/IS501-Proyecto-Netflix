4a3a71241271ae73e636cc338153bac55da66b22
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2019-02-25 23:55:26 -0600

    inicio
    avance

---
+++ Inicio.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+    <link href="css/inicio.css" type="text/css" rel="stylesheet">
+    <link href="css/all.css" type="text/css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="img/favicon.ico">
+
+</head>
+<body>
+          <header>
+                <nav class="navbar navbar-expand-md navbar-dark fixed-top " role="navigation">
+                 <div class="container">
+                             
+                            <ul class="navbar-nav ">
+                                <li class="nav-item active">
+                                        <a class="nav-item"  href="#">
+                                        <img class="letras" src="img/letras.png">
+                                        </a>
+                                </li>
+                            </ul>
+
+
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarCollapse" style="float=left">
+                              <ul class="navbar-nav nav-margen">
+                                <li class="nav-item active">
+                                     <a class="nav-link" href=""><b>Inicio</b></a>      
+                                </li>
+
+                                <li class="nav-item active">
+                                     <a class="nav-link" href="">Series</a>      
+                                </li>
+
+                                <li class="nav-item active">
+                                     <a class="nav-link" href="">Películas</a>      
+                                </li>
+
+                                <li class="nav-item active">
+                                     <a class="nav-link" href="">Agregados recientemente</span></a>      
+                                </li>
+
+                                
+                              </ul>
+                              
+                            </div>
+
+
+                            <ul class="navbar-nav  nav-margen">
+                                <li class="nav-item active">
+                                     <a href=""><i class="fas fa-search iconos"></i></a>      
+                                </li>
+
+                                <li class="nav-item active">
+                                  <a class="nav-link" href="#" style="padding-rigth:25px; padding-left:25px">NIÑOS</a>
+                                </li>
+
+                                <li class="nav-item active">
+                                   <a href=""><i class="fas fa-bell iconos" ></i></a>
+                                </li>
+
+                              <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+                                   <div class="btn-group" role="group" style="margin-left: 18px">
+                                        <button id="btnGroupDrop1" type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:white">
+                                             <img style="width:30px" src="img/profile.png">
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                             <img style="width:30px; float:left; margin-rigth:5px" src="img/profile.png"><a class="dropdown-item" href="#">Cuenta2</a>
+                                             <a class="dropdown-item" href="#">Niños</a>
+                                             <a class="dropdown-item" href="#">Administrar perfiles</a>
+                                             <hr>
+                                             <a class="dropdown-item" href="#"><b>Cuenta</b></a>
+                                             <a class="dropdown-item" href="#"><b>Centro de Ayuda</b></a>
+                                             <a class="dropdown-item" href="#"><b>Cerrar sesión en Netflix</b></a>
+                                             
+
+                                        </div>
+                                   </div>
+                              </div>
+                                
+                            </ul>
+
+                 </div>
+                </nav>
+          </header>
+
+          <section>
+           <img src="img/series/vikingos/9.jpg" style="width:100%; height:800px; position:absolute"> 
+               <div class="descripcion1">
+                    <div>
+                         <h4 style="color:white">ORIGINAL DE <b>NETFLIX</b></h4>
+                         <img src="img/series/vikingos/logo.png" style="width:85%">
+                    </div>
+                    <br>
+                    <div class="btn" role="group">
+                         <button type="button" id="btn-1y2" class="btn btn-secondary"><i class="fas fa-play"></i> Reproducir</button>
+                         <button type="button" id="btn-1y2" class="btn btn-secondary"><i class="fas fa-plus"></i> Mi Lista</button>
+                    </div>
+                    <br>
+                    <div style="width:90%; padding-top:3%; font-weight:bold">                       
+                         <h5><b>Ver la temporada 4</b></h5>
+                         <p style="text-align : justify;">Seremos testigos del regreso de Ragnar a Wessex. Por otro lado, Lagertha tratará de mantenerse en el poder en Kattegat y comenzará una relación amorosa. Además, veremos a Bjorn surcando el Mediterráneo.</p>                     
+                    </div>
+
+               </div>
+
+                    <div class="carousel1">
+                         <h5><b>Populares en Netflix</b></h5>
+                    </div>
+               
+          </section>
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/controlador-inicio.js"></script>
+   
+
+</body>
+</html>
