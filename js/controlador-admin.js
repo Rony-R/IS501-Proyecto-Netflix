d25086f3fc9e46e254d99f2eacca29c856c3e7ad
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2019-05-01 14:51:18 -0600

    ...

--- js/controlador-admin.js
+++ js/controlador-admin.js
@@ -3,34 +3,55 @@ $(document).ready(function(){
         $('#btn-peliculas').click(function() {
             $('#form2').replaceWith(`<form id="form1">
                                         <div class="form-group">
-
-                                            <div class="row" style="margin-left:auto; margin-right:auto">         
+                                                <div class="row" style="margin-left:auto; margin-right:auto">         
                                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
-                                                    <input class="form-control" type="text" placeholder="Nombre" id="txt-nombre">
+                                                    <input class="form-control" type="text" placeholder="Nombre Película" id="txt-nombre">
                                                 </div>
-
-                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
+                    
+                                                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
                                                     <input type="number" class="form-control" placeholder="Duración" id="txt-duracion">
                                                 </div>
-
-                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">                  
+            
+                                                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">                  
                                                     <select name="idioma" id="idioma" class="form-control"></select>
                                                 </div>
-
-                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">                  
-                                                    <select name="categoria" id="categoria" class="form-control"></select>
+            
+                                                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
+                                                    <input class="form-control" type="text" placeholder="Edad Público" id="txt-edad">
                                                 </div>
-
+            
                                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
-                                                    <textarea name="descripcion" id="descripcion" cols="30" rows="10" class="form-control">Descripción</textarea><br>
-                                                </div>   
-
+                                                    <input class="form-control" type="text" placeholder="Nombre Director" id="txt-director">
+                                                </div>
+            
                                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
-                                                    <select size=6 name="genero" id="genero" class="form-control" placeholder="Duración"></select> 
+                                                    <input class="form-control" type="text" placeholder="URL imagen miniatura" id="url-img">
+                                                </div>
+            
+            
+            
+                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">                  
+                                                    <select  name="estudio" id="estudio" class="form-control"></select>
+                                                </div>
+            
+                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
+                                                    <input class="form-control" type="text" placeholder="URL" id="txt-url">
+                                                </div>
+            
+                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
+                                                    <textarea name="descripcion" id="descripcion" cols="30" rows="3" class="form-control">Descripción</textarea><br>
+                                                </div> 
+                    
+                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">                  
+                                                    <select size=4 name="categoria" id="categoria" class="form-control"></select>
                                                 </div>
                                         
                                                 
-                     
+                    
+                                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px; float:right">
+                                                    <select size=4 name="genero" id="genero" class="form-control" placeholder="Duración"></select> 
+                                                </div>
+                                                                    
                                             </div>
                                         </div>
 
@@ -39,49 +60,54 @@ $(document).ready(function(){
 
     //series
     $('#btn-series').click(function() {
-        $('#form1').replaceWith(`<form id="form2">
+        $('#form1').replaceWith(`<form id="form2" style="width:90%">
                                     <div class="form-group">
 
-                                        <div class="row" style="margin-left:auto; margin-right:auto">         
+                                        <div class="row" style="margin-left:auto; margin-right:auto"> 
+                                          
                                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
                                                 <input class="form-control" type="text" placeholder="Nombre Episodio" id="txt-nombre">
                                             </div>
 
-                                            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12" style="margin-bottom:5px">
+                                            <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
                                                 <input class="form-control" type="number" placeholder="nº temporada" id="txt-temporada">
                                             </div>
 
-                                            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12" style="margin-bottom:5px">
-                                                <input class="form-control" type="number" placeholder="nº episodio" id="txt-episodio">
+                
+
+                                            <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
+                                                <input type="number" class="form-control" placeholder="Duración min" id="txt-duracion">
                                             </div>
 
-                                            
+                                            <div class="col-xl-2 col-lg-2 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
+                                                    <input class="form-control" type="text" placeholder="Edad Público" id="txt-edad">
+                                            </div>
+                                    
 
                                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
-                                                <input type="number" class="form-control" placeholder="Duración" id="txt-duracion">
+                                                <input class="form-control" type="text" placeholder="URL episodio" id="txt-url">
                                             </div>
 
-                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">                  
-                                                <select name="idioma" id="idioma" class="form-control"></select>
+                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
+                                                <input class="form-control" type="text" placeholder="URL imagen miniatura" id="url-img">
                                             </div>
 
-                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px; float:left">
+                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
                                                 <textarea name="descripcion" id="descripcion" cols="30" rows="3" class="form-control">Descripción</textarea><br>
                                             </div> 
-
 
                                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">                  
                                                 <select size=4 name="categoria" id="categoria" class="form-control"></select>
                                             </div>
                                     
-                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px; float:left">
+                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
                                                 <select size=4 name="genero" id="genero" class="form-control" ></select> 
                                             </div>
 
-                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px; float:left">
+                                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style="margin-bottom:5px">
                                                 <select size=4 name="personas" id="personas" class="form-control" ></select> 
                                             </div>
-  
+                                        
                                                                 
                                         </div>
                                     </div>
