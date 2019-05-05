69eab5a71e079ee66ff5495ed960e4195821818a
SigneLuque17 <31870779+SigneLuque17@users.noreply.github.com>
2019-05-05 05:16:42 -0600

    uniendo

--- scripts/USER.sql
+++ scripts/USER.sql
@@ -1,16 +1,16 @@
 --Crear nuevo usuario(esquema) con el password "PASSWORD" 
-CREATE USER NET6
+CREATE USER NETFLIX
   IDENTIFIED BY "oracle"
   DEFAULT TABLESPACE USERS
   TEMPORARY TABLESPACE TEMP;
 --asignar cuota ilimitada al tablespace por defecto  
-ALTER USER NET6 QUOTA UNLIMITED ON USERS;
+ALTER USER NETFLIX QUOTA UNLIMITED ON USERS;
 
 --Asignar privilegios basicos
-GRANT create session TO NET6;
-GRANT create table TO NET6;
-GRANT create view TO NET6;
-GRANT create any trigger TO NET6;
-GRANT create any procedure TO NET6;
-GRANT create sequence TO NET6;
-GRANT create synonym TO NET6;
+GRANT create session TO NETFLIX;
+GRANT create table TO NETFLIX;
+GRANT create view TO NETFLIX;
+GRANT create any trigger TO NETFLIX;
+GRANT create any procedure TO NETFLIX;
+GRANT create sequence TO NETFLIX;
+GRANT create synonym TO NETFLIX;
